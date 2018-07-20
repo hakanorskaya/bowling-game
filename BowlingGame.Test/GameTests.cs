bec70054189e61f44691ea74e7333552bbb4f127
Hakan Örskaya <orskaya@gmx.com>
2018-07-20 13:55:44 +0300

    Initial commit.

---
+++ BowlingGame.Test/GameTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGame.Test
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
