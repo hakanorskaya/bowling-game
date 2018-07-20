119e637c6ea8c6fcffb859914f250789bf9c171c
Hakan Örskaya <orskaya@gmx.com>
2018-07-20 14:42:54 +0300

    Get twenty points if spared once and then all zero'es rolled.

--- BowlingGame.Test/GameTests.cs
+++ BowlingGame.Test/GameTests.cs
@@ -16,6 +16,7 @@ namespace BowlingGame.Test
         [TestMethod]
         public void Get_Zero_Points_If_All_Zeroes_Rolled()
         {
+            // -- -- -- -- -- -- -- -- -- --
             for (int i = 0; i <= 19; i++)
             {
                 _game.Roll(0);
@@ -27,9 +28,27 @@ namespace BowlingGame.Test
         [TestMethod]
         public void Get_Twenty_Points_If_All_Ones_Rolled()
         {
+            // 11 11 11 11 11 11 11 11 11 11
             for (int i = 0; i <= 19; i++)
             {
                 _game.Roll(1);
+            }
+
+            Assert.AreEqual(20, _game.Score);
+        }
+
+        [TestMethod]
+        public void Get_Twenty_Points_If_Spared_Once_And_Then_All_Zeroes_Rolled()
+        {
+            // 5/ 5- -- -- -- -- -- -- -- --
+            _game.Roll(5);
+            _game.Roll(5);
+            _game.Roll(5);
+
+            // Roll zero, 17 times.
+            for (int i = 0; i <= 16; i++)
+            {
+                _game.Roll(0);
             }
 
             Assert.AreEqual(20, _game.Score);
--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -2,11 +2,37 @@
 {
     public class Game
     {
-        public int Score { get; set; }
+        private readonly int[] _rolls = new int[21]; // Since when last frame is spare or strike max possible roll count is 21
+        private int _currentRollIndex;
+
+        public int Score
+        {
+            get
+            {
+                var totalScore = 0;
+                var rollIndex = 0;
+                for (int frame = 0; frame <= 9; frame++) // 10 frames
+                {
+                    if (_rolls[rollIndex] + _rolls[rollIndex + 1] == 10) // if spare
+                    {
+                        totalScore += _rolls[rollIndex] + _rolls[rollIndex + 1] + _rolls[rollIndex + 2];
+                    }
+                    else
+                    {
+                        totalScore += _rolls[rollIndex] + _rolls[rollIndex + 1];
+                    }
+
+                    rollIndex += 2;
+                }
+
+                return totalScore;
+            }
+        }
 
         public void Roll(int pins)
         {
-            Score += pins;
+            _rolls[_currentRollIndex] = pins;
+            _currentRollIndex++;
         }
     }
 }
